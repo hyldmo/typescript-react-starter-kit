aa3db111b0b5166dd59baff1c0e7afd920955fa6
eivindh <eivind_h@outlook.com>
2017-12-05 20:19:27 +0100

    Added more tests

--- __tests__/postinstall.test.ts
+++ __tests__/postinstall.test.ts
@@ -1,11 +1,76 @@
-import { copy, mkdir, remove } from 'fs-extra'
+import { copy, mkdir, readdir, remove } from 'fs-extra'
 import * as path from 'path'
 
-it('copies run scripts to package.json', async (done) => {
-	// Arrange
+describe('postinstall', () => {
+	afterAll(async (done) => {
+		const testDirectory = path.join(__dirname, 'testfiles')
+		const dirs = (await readdir(testDirectory))
+			.filter(dir => dir.indexOf('postinstall-test') >= 0)
+			.map(dir => remove(path.join(testDirectory, dir)))
+
+		await Promise.all(dirs)
+
+		done()
+	})
+
+	it('copies run scripts to package.json', async (done) => {
+		// Arrange
+		const { scriptsFolder, testPath } = await makeFolders('package.json')
+
+		// Act
+		require(path.resolve(scriptsFolder, 'scripts/postinstall'))
+
+		// Assert
+		const expected = require(path.resolve(__dirname, '../package.json'))
+		delete expected.scripts.postinstall
+		delete expected.scripts.prepublishOnly
+		const result = require(path.resolve(testPath, 'package.json'))
+		expect(expected.scripts).toEqual(result.scripts)
+
+		done()
+	})
+
+	it('throws if there are duplicate keys', async (done) => {
+		// Arrange
+		const { scriptsFolder } = await makeFolders('package-withscripts.json')
+
+		// Act
+		const func = () => require(path.resolve(scriptsFolder, 'scripts/postinstall'))
+
+		// Assert
+		expect(func).toThrow()
+
+		done()
+	})
+
+	it('does not add non-build scripts', async (done) => {
+		// Arrange
+		const { scriptsFolder, testPath } = await makeFolders('package.json')
+
+		// Act
+		require(path.resolve(scriptsFolder, 'scripts/postinstall'))
+
+		// Assert
+		const result = require(path.resolve(testPath, 'package.json'))
+		const actualKeys = Object.keys(result.scripts);
+
+		['prepublishOnly', 'postinstall'].forEach(key => {
+			expect(actualKeys).not.toContain(key)
+		})
+
+		done()
+	})
+})
+
+type FolderResult = {
+	scriptsFolder: string
+	testPath: string
+}
+
+async function makeFolders (filename: string): Promise<FolderResult> {
 	const origPackagePath = path.resolve(__dirname, '../package.json')
 	const packageName = require(origPackagePath).name
-	const testPath = path.join(__dirname, 'postinstall-test')
+	const testPath = path.join(__dirname, `testfiles/postinstall-test${Math.round(Math.random() * Math.pow(2, 16))}`)
 	const modulePath = path.join(testPath, 'node_modules')
 	const scriptsFolder = path.join(modulePath, packageName)
 
@@ -17,24 +82,16 @@ it('copies run scripts to package.json', async (done) => {
 
 	await copy(origPackagePath, path.resolve(scriptsFolder, 'package.json'))
 	await copy(
-		path.resolve(__dirname, 'package-testfile.json'),
+		path.resolve(__dirname, `testfiles/${filename}`),
 		path.resolve(testPath, 'package.json')
 	)
 	await copy(
 		path.resolve(__dirname, '../scripts'),
 		path.join(scriptsFolder, 'scripts/')
 	)
+	return {
+		scriptsFolder,
+		testPath
+	}
+}
 
-	// Act
-	require(path.resolve(scriptsFolder, 'scripts/postinstall'))
-
-	// Assert
-	const expected = require(path.resolve(__dirname, '../package.json'))
-	delete expected.scripts.postinstall
-	const result = require(path.resolve(testPath, 'package.json'))
-	expect(expected.scripts).toEqual(result.scripts)
-
-	// Cleanup
-	await remove(testPath)
-	done()
-})
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -11,12 +11,22 @@ function overwriteScripts () {
     const destPkgFilename = path.resolve(__dirname, '../../../package.json');
     const destPkg = require(destPkgFilename);
 
-
-    destPkg.scripts = Object.assign({}, destPkg.scripts, sourcePkg.scripts)
+    const sourceKeys = Object.keys(sourcePkg.scripts);
+    const destKeys = Object.keys(destPkg.scripts)
+    const duplicateKeys = Object.keys(sourcePkg.scripts).filter(key => destKeys.some(dstKey => dstKey === key))
+    if (duplicateKeys.length > 0)
+        throw new Error(`Duplicate keys found in package.json found. Please remove "${duplicateKeys.join('", "')}" from the "scripts" object in package.json`)
 
     if (process.env.NODE_ENV !== 'test')
         console.info(pkgName + ': Overwriting package.json scripts...')
 
+    const excludeKeys = ['prepublishOnly', 'postinstall']
+    Object.keys(sourcePkg.scripts).forEach(key => {
+        if (excludeKeys.indexOf(key) >= 0)
+            delete sourcePkg.scripts[key]
+    })
+
+    destPkg.scripts = Object.assign({}, destPkg.scripts, sourcePkg.scripts)
     fs.writeJSON(destPkgFilename, destPkg, { spaces: 2 })
 }
 
