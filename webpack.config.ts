8da2af46b7112e8d8efba4f97e8072a0d7ac4aec
eivindh <eivind_h@outlook.com>
2018-03-17 17:31:20 +0100

    Added webpack 4 support

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CheckerPlugin } = require('awesome-typescript-loader')
 const webpack = require('webpack')
 
 const packageJSON = require('./package.json')
@@ -12,57 +11,59 @@ module.exports = {
 
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: '[name][hash].js'
+		filename: '[name]-[hash].js'
 	},
 
 	devServer: { port },
 
 	resolve: {
-		extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.pcss', '.css']
+		extensions: ['.ts', '.tsx', '.js', '.pcss', '.css']
 	},
 
 	module: {
 		rules: [
 			{
-				loader: 'css-loader',
-				options: {
-					importLoaders: 1,
-					sourceMap: true
-				}
+				test: /\.tsx?$/,
+				loaders: ['ts-loader']
 			},
 			{
-				loader: 'postcss-loader',
-				options: {
-					sourceMap: true,
-					config: {
-						path: './postcss.config.js'
-					}
-				}
+				test: /\.pcss$/,
+				use: ['css-loader', 'postcss-loader'].map(loader => ({
+					loader,
+					options: { sourceMap: true }
+				}))
 			}
 		]
 	},
 
 	plugins: [
-		new CheckerPlugin(),
 		new HtmlWebpackPlugin({
 			title: packageJSON.name
 				.split('-')
-				.map(name => name.charAt(0).toUpperCase() + name.slice(1))
+				.map((name) => name.charAt(0).toUpperCase() + name.slice(1))
 				.join(' '),
 			version: packageJSON.version,
 			template: 'static/index.ejs'
 		}),
 		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 			'process.env.PACKAGE_NAME': JSON.stringify(packageJSON.name),
 			'process.env.PACKAGE_VERSION': JSON.stringify(packageJSON.version)
 		}),
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			parallel: true
-		})
+		new webpack.optimize.OccurrenceOrderPlugin()
 	],
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					chunks: 'initial',
+					test: path.resolve(__dirname, 'node_modules'),
+					name: 'vendor',
+					enforce: true
+				}
+			}
+		}
+	},
 
 	stats: {
 		assets: true,
--- webpack.development.config.ts
+++ webpack.development.config.ts
@@ -1,52 +1,22 @@
 ﻿const CONFIG = require('./webpack.config.ts')
 const webpack = require('webpack')
 
+CONFIG.module.rules[1].use.unshift('style-loader')
+
 module.exports = Object.assign(CONFIG, {
+	mode: 'development',
 	devtool: 'cheap-eval-source-map',
 
 	devServer: {
 		historyApiFallback: true,
 		port: CONFIG.devServer.port,
-		hotOnly: true,
+		hot: true,
 		overlay: true,
 		stats: CONFIG.stats
 	},
 
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loaders: ['awesome-typescript-loader']
-			},
-			{
-
-				test: /\.pcss$/,
-				exclude: ['node_modules'],
-				use: [
-					{
-						loader: 'style-loader',
-						options: {
-							sourceMap: true
-						}
-					},
-					...CONFIG.module.rules
-				]
-			}
-		]
-	},
-
 	plugins: [
 		...CONFIG.plugins,
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'vendor',
-			minChunks: module => module.context && module.context.includes('node_modules')
-		}),
-
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'manifest',
-			minChucks: Infinity
-		}),
-		new webpack.NamedModulesPlugin(),
 		new webpack.HotModuleReplacementPlugin()
 	]
 })
