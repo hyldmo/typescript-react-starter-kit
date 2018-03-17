2e5037d25269d9d8f3e6348309fa8eb17285c021
eivindh <eivind_h@outlook.com>
2018-03-17 14:00:29 +0100

    Fixed linting in webpack conf files

---
+++ webpack.production.config.ts
@@ -0,0 +1,29 @@
+const CONFIG = require('./webpack.config.ts')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = Object.assign(CONFIG, {
+	devtool: 'source-map',
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader'
+			},
+			{
+				test: /\.pcss$/,
+				exclude: ['node_modules'],
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: CONFIG.module.rules
+				})
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('styles.css'),
+		...CONFIG.plugins
+	]
+})
