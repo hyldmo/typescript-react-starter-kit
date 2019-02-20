401bd034dc5dfc389782038ee02cd6705568a882
Eivind Hyldmo <3465788+hyldmo@users.noreply.github.com>
2019-02-20 16:09:19 +0100

    Greenkeeper/monorepo.react 20190214213100 (#103)
    Closes #100 #98

--- src/components/Routes/About.tsx
+++ src/components/Routes/About.tsx
@@ -1,26 +1,24 @@
 import { Actions } from 'actions'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { State } from 'types'
 
 type Props = ReturnType<typeof mapStateToProps> & typeof dispatchToProps
 
-class About extends React.Component<Props> {
-	componentDidMount () {
-		this.props.fetchVersion('https://raw.githubusercontent.com/hyldmo/typescript-react-starter-kit/master/package.json')
-	}
+const About: React.StatelessComponent<Props> = ({ version, fetchVersion }) => {
+	useEffect(() => {
+		fetchVersion('https://raw.githubusercontent.com/hyldmo/typescript-react-starter-kit/master/package.json')
+	})
 
-	render () {
-		return (
-			<>
-				<h1>About {process.env.PACKAGE_NAME}</h1>
-				<h2></h2>
-				<h2>
-					Version: {this.props.version}
-				</h2>
-			</>
-		)
-	}
+	return (
+		<>
+			<h1>About {process.env.PACKAGE_NAME}</h1>
+			<h2></h2>
+			<h2>
+				Version: {version}
+			</h2>
+		</>
+	)
 }
 
 const mapStateToProps = (state: State) => ({
