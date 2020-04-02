f25f4d27cabc22b4d1fb1a9e9dc573d114edb39a
hyldmo <eivind_h@outlook.com>
2020-04-02 20:27:20 +0200

    Updated action typings and react packages

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,5 @@ export * from './tracker'
 export type State = ReturnType<ReturnType<typeof reducers>>
 
 export type Selector<T> = (t: T) => unknown
+
+export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType[number]
